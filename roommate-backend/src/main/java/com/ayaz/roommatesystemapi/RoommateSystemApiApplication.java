f7e1d3c130ac179abce984cf472ec853c9a94ddb
AyazCiplak <97311304+AyazCiplak@users.noreply.github.com>
2023-06-30 15:37:18 -0400

    Spring+Java backend project init

---
+++ roommate-backend/src/main/java/com/ayaz/roommatesystemapi/RoommateSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.ayaz.roommatesystemapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoommateSystemApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoommateSystemApiApplication.class, args);
+    }
+
+}
---
+++ roommate-backend/src/test/java/com/ayaz/roommatesystemapi/RoommateSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayaz.roommatesystemapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoommateSystemApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
